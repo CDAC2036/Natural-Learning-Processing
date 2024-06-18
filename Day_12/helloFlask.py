3ab216b785e19b58b3b60dbec6cda2772d9e2c7e
CDAC2036 <cdac2036@gmail.com>
2024-06-18 08:58:33 +0530

    Day_12 Codes - Flask Applications

--- Day_12/helloFlask.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template
-app=Flask(__name__)
-@app.route('/hello/<user>')
-def index(user):
-    return render_template('hello.html',name=user)
-
-if __name__=='__main__':
-    app.run(debug=True)
-
